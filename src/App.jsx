3211ad7c76762512c1927be1338938b735493a22
Prabhash Singh <prabhash.amazon.lko@gmail.com>
2025-05-31 18:23:15 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   const [videoSrc, setVideoSrc] = useState(null);
@@ -11,11 +12,13 @@ function App() {
     if (file) {
       const url = URL.createObjectURL(file);
       setVideoSrc(url);
+      document.title=file.name;
     }
   };
 
   return (
     <>
+      <SpeedInsights />
       <div>
         <div className="card">
           <div className="logos">
@@ -23,12 +26,11 @@ function App() {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </div>
           <div className="header">
-            <h3 className="heading3">Video Player</h3>
+            <h3>Video Player</h3>
             <input
               type="file"
-              accept="video/mkv, video/*,.mkv"
+              accept="video/x-matroska,video/*,.mkv"
               onChange={handleChange}
-              className="file-selector"
             />
           </div>
         </div>
